44a8542192b62dc068696129dd4dd4b85ca3c884
sruti1502 <sruti1501@gmail.com>
2026-08-05 23:21:49 +0530

    Fix Prisma configuration for deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,10 @@
+import "dotenv/config";
 import { defineConfig } from "prisma/config";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is missing");
-}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
 
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 });
